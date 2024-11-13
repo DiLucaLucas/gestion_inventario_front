814a9c1111bc13bd9019e1fddeb92e457361c60e
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-11-13 15:56:08 -0300

    Funciones para redirigir a pantalla login y registro

--- src/pages/WelcomeScreen/WelcomeScreen.jsx
+++ src/pages/WelcomeScreen/WelcomeScreen.jsx
@@ -1,11 +1,21 @@
 import { Button } from "primereact/button";
+import { useNavigate } from "react-router-dom";
 
 export const WelcomeScreen = () => {
+  // Hook para redirigir
+  const navigate = useNavigate();
+
+  // Funcion para Login
+  const handleLoginClick = () => {
+    navigate("/login");
+  };
+
+  // Función para Registro
+  const handleRegisterClick = () => {
+    navigate("/register");
+  };
   return (
-    <div
-      className="bg-gray-900 flex align-items-center justify-content-center"
-      style={{ height: "100vh" }}
-    >
+    <div className="bg-gray-900 flex align-items-center justify-content-center" style={{ height: "100vh" }} >
       <div className="text-center">
         <h1 className="text-teal-100 font-bold text-4xl">
           Proyecto Final: Laboratorio Full Stack + Programación Backend
@@ -17,16 +27,8 @@ export const WelcomeScreen = () => {
           Stack y Programacion Backend
         </p> */}
         <div className="flex align-items-center justify-content-center mt-4">
-          <Button
-            label="Iniciar Sesión"
-            icon="pi pi-sign-in"
-            className="bg-teal-500 mr-3 hover:bg-teal-600 border-none"
-          />
-          <Button
-            label="Registrarse"
-            icon="pi pi-user-plus"
-            className="bg-teal-500 hover:bg-teal-600 border-none"
-          />
+          <Button label="Iniciar Sesión" icon="pi pi-sign-in" className="bg-teal-500 mr-3 hover:bg-teal-600 border-none" onClick={handleLoginClick} />
+          <Button label="Registrarse" icon="pi pi-user-plus" className="bg-teal-500 hover:bg-teal-600 border-none" onClick={handleRegisterClick} />
         </div>
       </div>
     </div>
