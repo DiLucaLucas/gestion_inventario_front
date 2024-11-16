3dae012c95707818e2084c3dc9b05688ce7fcc91
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-11-15 23:30:07 -0300

    Fix LoginScreen Input Password

--- src/pages/LoginScreen/LoginScreen.jsx
+++ src/pages/LoginScreen/LoginScreen.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
-import { Password } from "primereact/password";
 import { Avatar } from "primereact/avatar";
 
 export default function LoginScreen() {
+  const navigate = useNavigate();
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleLogin = () => {
+    // Simular validación de credenciales
+    if (credentials.username === "admin" && credentials.password === "1234") {
+      console.log("Login exitoso!");
+
+      // Redirigir al Home
+      navigate("/home");
+    } else {
+      console.log("Credenciales inválidas");
+      alert("Usuario o contraseña incorrectos");
+    }
+  };
+  
   return (
     <div className="background-image flex align-items-center justify-content-center min-h-screen">
       <div className="bg-dark p-4 w-full rounded sm:w-25rem">
@@ -20,21 +41,27 @@ export default function LoginScreen() {
           </p>
         </div>
 
-        {/* Contenedor de los inputs */}
         <div className="flex flex-column align-items-center w-full gap-4">
           <InputText
             id="email"
             type="text"
             className="w-full"
             placeholder="Ingresa tu correo"
+            value={credentials.username}
+            onChange={(e) =>
+              setCredentials({ ...credentials, username: e.target.value })
+            }
           />
 
-          {/* Ajuste en Password para que tome el ancho completo */}
-          <Password
+          <input
             id="password"
-            toggleMask
-            className="w-full"
+            type="password"
             placeholder="Contraseña"
+            className="p-inputtext p-component w-full custom-password mt-4"
+            value={credentials.password}
+            onChange={(e) =>
+              setCredentials({ ...credentials, password: e.target.value })
+            }
           />
         </div>
 
@@ -49,8 +76,8 @@ export default function LoginScreen() {
 
         <Button
           label="Ingresar"
-          className="w-full p-button-success mt-4"
-          style={{ backgroundColor: "#4BFFB3", color: "#3D3D3D" }}
+          className="w-full p-button-success mt-4 btn-success-custom"
+          onClick={handleLogin}
         />
       </div>
     </div>
