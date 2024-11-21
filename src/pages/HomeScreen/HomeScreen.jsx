77a9331ac4c1f85eeef0a38471c3b4c015adafda
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-11-21 17:09:47 -0300

    `Feat: HomeScreen` | Implementación de HomeScreen donde se añaden los componentes que forman parte de la misma.

--- src/pages/HomeScreen/HomeScreen.jsx
+++ src/pages/HomeScreen/HomeScreen.jsx
@@ -1,7 +1,24 @@
-export default function HomeScreen() {
-  return (
-    <div className="home-screen">
-      <h1>Bienvenido al Home</h1>
-    </div>
-  );
-}
+import MenuComponent from '../../components/MenuComponent';
+import CardComponent from '../../components/shared/CardComponent';
+
+const activities = [
+  { productName: 'Frenos Shimano M6100', action: 'created', time: 'Hace 2 horas' },
+  { productName: 'Cadena KMC Gold 12v', action: 'deleted', time: 'Hace 1 día' },
+  { productName: 'Cubiertas Maxxis ikon 29 TR', action: 'deleted', time: 'Hace 1 día' },
+  { productName: 'Bicicleta Giant Talon 3', action: 'deleted', time: 'Hace 1 día' },
+];
+
+const HomeScreen = () => {
+    return (
+        <div className='bg-gray-100 h-screen flex flex-column'>
+          <div className='w-full'>
+            <MenuComponent />
+          </div>
+            <div className='flex justify-content-center align-items-center flex-grow-1'>
+              <CardComponent activities={activities}/>
+            </div>
+        </div>
+    );
+};
+
+export default HomeScreen;
