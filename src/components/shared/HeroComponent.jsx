a217b3306eabd7b8ca06748e7e069058e3bdc669
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-11-21 17:07:00 -0300

    `Feat: HeroComponent` | Se añadio breve descripción del proyecto.

---
+++ src/components/shared/HeroComponent.jsx
@@ -0,0 +1,22 @@
+export const HeroComponent = () => {
+  return (
+    <div className="p-6 justify-center align-items-center">
+      <div className="text-center p-6 justify-center align-items-center">
+          <h1 className="text-success-green font-bold text-3xl mb-3 md:text-4xl">
+            Bienvenidos a J-Log
+          </h1>
+        <div>
+          <p className="text-xl md:text-2xl font-bold text-almost-black">
+            Somos Jeronimo Huincaman y Lucas Di Luca, estudiantes de Tecnicatura
+            Superior Full Stack en ITS. Este proyecto fue realizado con el
+            objetivo de demostrar los conocimientos adquiridos en Laboratorio
+            Full Stack y Programación Backend. La aplicación está destinada al
+            manejo de inventario y usuarios de un entorno.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroComponent;
